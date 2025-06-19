30888b3c6368a35bc6bc488da4ebcc83907f3d57
BMi <bmi.dev@redox.ws>
2025-06-19 23:44:00 +0200

    Bump vescpp, update scripts

--- scripts/flashAll.sh
+++ scripts/flashAll.sh
@@ -4,6 +4,7 @@ can_port=""
 dir=""
 ids=""
 uuids=""
+
 if [ -z "$1" -o "$1" = "bench" ]; then
   echo "Config: Bench"
   can_port="can0"
@@ -15,13 +16,13 @@ if [ -z "$1" -o "$1" = "bench" ]; then
     uuids="$uuids 0x4e00260013504b4d31383120"  # 14
     uuids="$uuids 0x310036000547323132343933"  # 15
     uuids="$uuids 0x1f00330014504b4d31383120"  # 16
-    dir="$dir/edu/"
+    fw_file="../edu.bin"
   elif [ -z "$2" -o "$2" = "bmi_a50s" ]; then
     can_port="can3"
     uuids="$uuids 0x2d002f001247333034333237"  # 11
     uuids="$uuids 0x2d0040001247333034333237"  # 12 
     uuids="$uuids 0x250022001147333034333237"  # 13
-    dir="$dir/a50s/"
+    fw_file="../a50s_v23c_8s.bin"
   else
     ids=`seq 11 16`
   fi
@@ -30,28 +31,25 @@ elif [ "$1" = "explorer" ]; then
   can_port="can2"
   dir="explorer"
   ids=`seq 11 16`
+  fw_file="../edu.bin"
 else
   echo "Usage: $0 [explorer|bench]. Abort"
   exit 0
 fi
 
-cmd_base="./cli/vescpp_cli -P $can_port -D $dir"
+cmd_base="./cli/vescpp_cli -f -P $can_port"
 
 if [ "$ids" != "" ]; then 
   for i in $ids; do 
-    for cnf in "app" "motor" "custom"; do
-      cmd="$cmd_base -i $i save_conf $cnf"
-      echo $cmd
-      $cmd;
-    done
+    cmd="$cmd_base -i $i flash_fw app $fw_file"
+    echo $cmd
+    $cmd;
   done
 elif [ "$uuids" != "" ]; then
   for u in $uuids; do 
-    for cnf in "app" "motor" "custom"; do
-        cmd="$cmd_base -u $u save_conf $cnf"
-        echo $cmd
-        $cmd;
-      done
+    cmd="$cmd_base -u $u flash_fw app $fw_file"
+    echo $cmd
+    $cmd;
   done
 else
   echo "IDs or UUIDs please !"
--- scripts/loadAll.sh
+++ scripts/loadAll.sh
@@ -6,8 +6,14 @@ ids=""
 if [ -z "$1" -o "$1" = "bench" ]; then
   echo "Config: Bench"
   can_port="can0"
-  dir="bench"
   ids=`seq 11 16`
+  dir="bench"
+  if [ -z "$2" -o "$2" = "bmi_edu" ]; then
+    dir="$dir/edu/"
+  elif [ -z "$2" -o "$2" = "bmi_a50s" ]; then
+    can_port="can3"
+    dir="$dir/a50s/"
+  fi
 elif [ "$1" = "explorer" ]; then
   echo "Config: Explorer"
   can_port="can2"
@@ -17,13 +23,13 @@ else
   echo "Usage: $0 [explorer|bench]. Abort"
   exit 0
 fi
-
 cmd_base="./cli/vescpp_cli -f -P $can_port"
 
 for i in $ids; do 
   for cnf in "motor" "custom" "app"; do
     cnff="`ls -Art $dir/${cnf}_conf_${i}_*.json 2> /dev/null | tail -n 1`"
     if [ -z "$cnff" ]; then
+      echo "id $i conf $cnf not found in $dir"
       continue
     fi
     uuidb=`cat $cnff | jq -r '.["info"]["uuid"]["bytes"] | .[]'`
@@ -31,8 +37,8 @@ for i in $ids; do
     for ub in $uuidb; do
       uuid="${uuid}`printf "%02x" $ub`"
     done
-    cmd="$cmd_base -i $i -u $uuid load_conf $cnf $cnff"
-    #echo $cmd
+    cmd="$cmd_base -u $uuid load_conf $cnf $cnff"
+    echo $cmd
     $cmd;
   done
 done
